81575a28ec87fecdbdafabd50e2146cdccaa9b54
Mohamad Alfian Ramadhan <56055855+nspbdz@users.noreply.github.com>
2022-04-07 13:36:13 +0700

    datawarga

--- src/config/api.js
+++ src/config/api.js
@@ -3,8 +3,8 @@ import axios from "axios";
 // Create base URL API
 export const API = axios.create({
   // baseURL: "http://localhost:5000/api/v1/",
-  baseURL: "http://127.0.0.1:8000/api/",
-  // baseURL: "https://warga-app.herokuapp.com/api/",
+  // baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://warga-app.herokuapp.com/api/",
 
 
 });
